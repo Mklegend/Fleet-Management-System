dbf276d0d0b8855377d7a52d58787a9fcf9d0e88
Moazzam Qureshi <qureshimoazzam7@gmail.com>
2023-04-15 00:42:01 +0500

    Implemented BookingManager in Business Layer

--- Business/Implementation/BookingManager.cs
+++ Business/Implementation/BookingManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Business.Interface;
+using Data;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,41 @@ using System.Threading.Tasks;
 
 namespace Business.Implementation
 {
-    public class BookingManager
+    public class BookingManager : IBookingManager
     {
-        public BookingManager() { }
+        private readonly DatabaseContext dbContext;
+        public BookingManager(DatabaseContext databaseContext) {
+            dbContext= databaseContext;
+        }
+
+        public List<Booking> GetBookings() {
+            return dbContext.Booking.ToList();
+        }
+        public bool UpdateBooking(Booking booking) {
+
+            bool BookingExists = dbContext.Booking.Any(b => b.BookingId == booking.BookingId);
+            
+            if (BookingExists)
+            {
+                dbContext.Booking.Update(booking);
+            }
+            else { 
+                dbContext.Booking.Add(booking);
+            }
+            
+            int result = dbContext.SaveChanges();
+
+            if (result == 0) return false;
+
+            return true;
+        }
+        public bool DeleteBooking(Guid id) { 
+             bool BookingExists = dbContext.Booking.Any(b => b.BookingId == id);
+            if (BookingExists) {
+                dbContext.Booking.Remove(new Booking { BookingId = id });
+                return true;
+            }
+            return false;
+        }
     }
 }
