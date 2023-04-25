a3c59a2ba8414461e49958dabaac7d59145e2dc1
Moazzam Qureshi <qureshimoazzam7@gmail.com>
2023-04-25 13:13:00 +0500

    Added Custom Validation to Customer Form

--- FrontEnd/src/app/components/customer-form/customer-form.component.ts
+++ FrontEnd/src/app/components/customer-form/customer-form.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { Customer } from 'src/app/models/Customer';
 import { CustomerApiService } from 'src/app/services/customer-api.service';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-form',
@@ -12,17 +13,43 @@ import { CustomerApiService } from 'src/app/services/customer-api.service';
 export class CustomerFormComponent implements OnInit {
   form: FormGroup;
   @Input() customer: Customer;
-  @Input() close!:()=>void;
-  constructor(private customerApi: CustomerApiService, private notification:NzNotificationService) {
+  @Input() close!: () => void;
+  constructor(
+    private customerApi: CustomerApiService,
+    private notification: NzNotificationService
+  ) {
     this.customer = {} as Customer;
     this.form = new FormGroup({
       customerName: new FormControl('', [Validators.required]),
       phoneNumber: new FormControl('', [Validators.required]),
       address: new FormControl('', [Validators.required]),
-      cnic: new FormControl('', [Validators.required]),
-      licenseNumber: new FormControl('', [Validators.required]),
+      cnic: new FormControl('', [Validators.required,this.cnicValidator()]),
+      licenseNumber: new FormControl('', [Validators.required,this.licenseNumberValidator()]),
     });
   }
+
+  cnicValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const cnicRegex = /^(\d{5})-?(\d{7})-?(\d{1})$/;
+      const cnic = control.value;
+      if (cnic && !cnicRegex.test(cnic)) {
+        return { cnic: { value: cnic } };
+      }
+      return null;
+    };
+  }
+
+  licenseNumberValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const licenseRegex = /^[A-Z]{2}-\d{2}-\d{5}$/;
+      const license = control.value;
+      if (license && !licenseRegex.test(license)) {
+        return { 'licenseNumber': { value: license } };
+      }
+      return null;
+    };
+  }
+
   ngOnInit() {
     this.form.get('customerName')?.setValue(this.customer.customerName);
     this.form.get('phoneNumber')?.setValue(this.customer.phoneNumber);
@@ -43,15 +70,18 @@ export class CustomerFormComponent implements OnInit {
       formData.append('address', this.form.get('address')?.value);
       formData.append('cnic', this.form.get('cnic')?.value);
       formData.append('licenseNumber', this.form.get('licenseNumber')?.value);
-      
-      this.customerApi.updateCustomer(formData).subscribe(res=>{
-        if(res==true){
-          console.log("Customer Updated Successfully !")
-          this.notification.create('success', 'Success', 'Customer has been Updated Succesfully !');
+
+      this.customerApi.updateCustomer(formData).subscribe((res) => {
+        if (res == true) {
+          console.log('Customer Updated Successfully !');
+          this.notification.create(
+            'success',
+            'Success',
+            'Customer has been Updated Succesfully !'
+          );
           this.close();
         }
-      })
-
+      });
     } else {
       Object.values(this.form.controls).forEach((control) => {
         if (control.invalid) {
